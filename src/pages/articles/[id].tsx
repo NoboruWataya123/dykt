cd9eb9fe08eeb3735e99e37ed30fdb0c243da7a1
ddk <dulustan1996@gmail.com>
2022-11-27 14:05:30 +0900

    fix: undefined

--- src/pages/articles/[id].tsx
+++ src/pages/articles/[id].tsx
@@ -14,14 +14,14 @@ const Article = () => {
             <Container size="lg" py="xl" style={{ justifyContent: 'center', alignItems: 'center' }}>
                 <Card shadow="sm" radius="md" style={{ justifyContent: 'center', alignItems: 'center' }}>
                     <AspectRatio ratio={16 / 9}>
-                        <Image src={post.data?.images[0]?.url} alt={post.data?.title} height={600} width={800} radius="md" style={{ objectFit: 'cover' }} />
+                        {post.data?.images[0] && (
+                            <Image src={post.data?.images[0]?.url} alt={post.data?.title} height={600} width={800} style={{ objectFit: 'cover' }} />
+                        )}
                     </AspectRatio>
                     <Group position="left" className="bg-gray-300 p-4 my-4">
                         <Title>{post.data?.title}</Title>
                     </Group>
                     <Group position="left" className="bg-gray-200 p-4 my-4">
-                        <EditorContent editor={post.data?.content} />
-
                     </Group>
                 </Card>
             </Container>
